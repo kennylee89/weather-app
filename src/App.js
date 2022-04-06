1a0f93d301b38bc547ba760328cb89db2c37ee97
Kenny Lee <kennyhojinlee@gmail.com>
2022-04-05 21:38:26 -0500

    set useState and useEffect to get lat and lon coordinates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from "react";
+
 function App() {
+
+  const [lat, setLat] = useState([]);
+  const [lon, setLon] = useState([]);
+
+  useEffect(()=> {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      setLat(position.coords.latitude);
+      setLon(position.coords.longitude);
+    });
+    console.log(`lat is ${lat}, lon is ${lon}`);
+  })
+
+
   return (
     <div className="App">
       <p>Weather App</p>
